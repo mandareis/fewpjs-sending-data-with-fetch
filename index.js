91c76d3f98d96bdf74ba2d43dc288826409b81fc
mandareis <amanda@depaula.xyz>
2020-11-19 10:31:03 -0800

    commit tree 4a2fabb574692c7f84d9ae0656c2d3522562cc9d

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object.id);
+    });
+}
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   });
+
+// configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   }),
+// };
